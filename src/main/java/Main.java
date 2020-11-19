2eaf2ba932617f5426a23e5837bfb202aa549001
Igor Vinokur <igor@ivtech.co.il>
2020-11-19 18:25:06 +0200

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        System.out.println("Hello AI");
-//        WorldLoader loader = new WorldLoader("/Users/igorvinokur/Development/Dev/Study/Assignment1AI/src/main/resources/graph.json");
-//        Graph<Vertex, Edge> g = loader.loadWorld();
-//        System.out.println(g);
         Environment e = new Environment();
         e.start();
 
